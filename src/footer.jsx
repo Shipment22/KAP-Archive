d60c669b235c707bd760aaa9036dba48398bb224
Ben <ben@familysafeinternet.com>
2022-12-20 20:47:08 -0500

    Footer HTML

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,7 +1,29 @@
 function Footer() {
 	return (
-		<footer>
-			Footer
+		<footer className="Footer">
+			<div className="footer_inner-wrapper">
+				<h2>Pages</h2>
+				<ul>
+					<li><a href="/">Home</a></li>
+					<li><a href="/browse">Browse</a></li>
+					<li><a href="/search">Search</a></li>
+					<li><a href="/add">Add</a></li>
+					<li><a href="/about">About</a></li>
+					<li><a href="/admin">Admin</a></li>
+				</ul>
+				<h2>Other similar, cool, or useful projects</h2>
+				<ul>
+					<li><a href="https://willard.fun">Willard.fun</a></li>
+					<li><a href="https://khanalytics.bhavjit.com/">Khanalytics</a></li>
+					<li><a href="https://kestrogle.tk">Kestrogle</a></li>
+				</ul>
+				<h2>Credits</h2>
+				<ul>
+					<li><a href="https://iconarchive.com/show/flatastic-1-icons-by-custom-icon-design/information-icon.html">Info Icon</a> (<a href="http://www.customicondesign.com/">Custom Icon Design</a>)</li>
+					<li><a href="https://iconarchive.com/show/small-n-flat-icons-by-paomedia/key-icon.html">Key Icon</a>(<a href="http://www.paomedia.com/">paomedia</a>, <a href="https://github.com/paomedia">Github</a>)</li>
+					<li><a href="https://iconarchive.com/show/papirus-status-icons-by-papirus-team/view-private-icon.html">View private Icon</a>(<a href="https://github.com/PapirusDevelopmentTeam">Papirus Development Team</a>)</li>
+				</ul>
+			</div>
 		</footer>
 	)
 }
