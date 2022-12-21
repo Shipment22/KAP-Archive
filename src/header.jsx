6e63d314a1a8f4ad67bef0a980a6e0047623be6b
Ben <ben@familysafeinternet.com>
2022-12-21 06:22:54 -0500

    Link color classes

--- src/header.jsx
+++ src/header.jsx
@@ -7,7 +7,7 @@ function Header() {
 	return (
 		<header className="Header">
 			<div>
-				<a href="/">
+				<a href="/" className="green">
 					<img src={images.logo} alt="KAP Archive Logo" height="48" />
 					KAP Archive
 				</a>
@@ -15,15 +15,15 @@ function Header() {
 			<div className="main-nav_wrapper">
 				<nav className="main-nav" aria-label="Main navigation">
 					<ul>
-						<li><a href="/browse">Browse</a></li>
-						<li><a href="/search">Search</a></li>
-						<li><a href="/add">Add Programs</a></li>
-						<li><a href="/about">About</a></li>
+						<li><a href="/browse" className="rosewater">Browse</a></li>
+						<li><a href="/search" className="mauve">Search</a></li>
+						<li><a href="/add" className="maroon">Add Programs</a></li>
+						<li><a href="/about" className="yellow">About</a></li>
 					</ul>
 				</nav>
 			</div>
 			<div>
-				<a href="/admin">
+				<a href="/admin" className="blue">
 					Admin Login
 					<img src={images.adminLogin} alt="Admin login image" width="48" />
 				</a>
