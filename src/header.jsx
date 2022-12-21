c90c87e04b4e9d8197bfb548097c767e59f28098
Ben <ben@familysafeinternet.com>
2022-12-20 19:58:08 -0500

    Header HTML

--- src/header.jsx
+++ src/header.jsx
@@ -1,7 +1,33 @@
+const images = {
+	logo: '/assets/logo.svg',
+	adminLogin: '/assets/key.svg'
+}
+
 function Header() {
 	return (
-		<header>
-			Header
+		<header className="Header">
+			<div>
+				<a href="/">
+					<img src={images.logo} alt="KAP Archive Logo" height="48" />
+					KAP Archive
+				</a>
+			</div>
+			<div className="main-nav_container">
+				<nav className="main-nav" aria-label="Main navigation">
+					<ul>
+						<li><a href="/browse">Browse</a></li>
+						<li><a href="/search">Search</a></li>
+						<li><a href="/add">Add Programs</a></li>
+						<li><a href="/about">About</a></li>
+					</ul>
+				</nav>
+			</div>
+			<div>
+				<a href="/admin">
+					Admin Login
+					<img src={images.adminLogin} alt="Admin login image" width="48" />
+				</a>
+			</div>
 		</header>
 	)
 }
