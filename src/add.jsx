147434663197ce4e83d2673e85858a5124324ec9
Ben <ben@familysafeinternet.com>
2023-01-11 22:26:08 -0500

    Create an add page

---
+++ src/add.jsx
@@ -0,0 +1,21 @@
+/*
+ * Name:   Add
+ * For:    Main tag in the /add endpoint
+*/
+import AddForm from './add-form';
+import Program from './program';
+function Add(props) {
+	return (
+<main className="Main">
+	<h1>Program Adding Results</h1>
+	<div className="surface">
+		{props.programs?(
+			<div className="programs-grid">
+				{props.programs.map(program => (<Program key={program.id} {...program}/>))}
+			</div>
+		):(<AddForm />)}
+	</div>
+</main>)
+}
+
+export default Add;
