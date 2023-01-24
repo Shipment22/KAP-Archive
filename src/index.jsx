95001039b26303c9991e2d0b1137fc762ff4602d
Ben <ben@familysafeinternet.com>
2023-01-24 00:08:47 -0500

    General code cleaning and refactoring

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,23 @@
 import { renderToReadableStream } from "react-dom/server";
-
+// Import page components
 import Header from './components/header';
 import Footer from './components/footer';
+// Import pages
 import ErrorPage from './pages/error';
 import Home from './pages/home';
 import Add from './pages/add';
-
+// Import library functions for saving and retrieving
 import { saveProgram, savePrograms } from './libs/archivePrograms.jsx';
 import { getProgram, getPrograms } from './libs/retrievePrograms.jsx';
 
 function checkLoggedin(request) {
-    if (request.headers.get('cookie') === null || request.headers.get('cookie').match(/key=[^;]+/) === null) {
-        return null
-    }
-    return (Bun.hash(request.headers.get('cookie').match(/key=[^;]+/)[0].slice(4)) === 62864701388280) ? "true" : null
+    // Check the key cookie's hash and if it's right return true
+    if (request.headers.get('cookie') === null || request.headers.get('cookie').match(/key=[^;]+/) === null)
+        return null;
+    return Bun.hash(request.headers.get('cookie').match(/key=[^;]+/)[0].slice(4)) === 62864701388280 ? "true" : null;
 }
-
 async function renderPage(page, request) {
-    const loggedIn = checkLoggedin(request)
+    const loggedIn = checkLoggedin(request);
     return new Response(
     await renderToReadableStream(
     <html lang="en">
@@ -34,12 +34,10 @@ async function renderPage(page, request) {
         <body>
             <Header loggedIn={loggedIn} />
             {page.props ? (<page.body {...page.props}/>) : (<page.body />) }
-            
             <Footer />
         </body>
     </html>,
-    ),
-    );
+    ),);
 }
 async function renderError(error, request) {
     const loggedIn = checkLoggedin(request)
@@ -58,15 +56,12 @@ async function renderError(error, request) {
             <Footer />
         </body>
     </html>,
-    ), 
-    {
-        status: error.status
-    }
-    );
+    ), { status: error.status });
 }
-// Input a path and outputs a response with image headers
 function serveImage(path) {
-    return new Response(Bun.file(path), { headers: { "content-type": "image/"+path.split('.').reverse()[0] } });
+    // Sends an image with headers based on the provided path
+    return new Response(Bun.file(path), { 
+        headers: { "content-type": "image/"+path.split('.').reverse()[0] } });
 }
 export default {
     async fetch(request) {
@@ -74,58 +69,64 @@ export default {
         const { url, method } = request;
         // Get Pathname form url.
         const { pathname } = new URL(url);
+        // /s/id endpoint
         if (pathname.match(/^\/s\/[0-9]+/i)) {
+            // Save the program
             const program = await saveProgram(pathname.split('/').reverse()[0]);
-            console.log(program)
+            // Handle errors
             if (program.status !== 200) {
-                if (method === 'GET') {
-                    return renderError(program, request)
-                } else {
-                    return new Response('Error: ' + program.message, {
-                        status: program.status
-                    })
-                }
+                // GUI error page
+                if (method === 'GET') return renderError(program, request);
+                // Non-GUI error message
+                return new Response('Error: ' + program.message, {
+                    status: program.status });
             }
+            // Send the saved data
             return new Response(JSON.stringify(program), {
-                headers: { "content-type": "application/json" }
-            })        } if (pathname.match(/^\/g\/[0-9]+/i)) {
-            const program = await getProgram(pathname.split('/').reverse()[0])
-            return new Response(JSON.stringify(program), {
-                headers: { "content-type": "application/json" }
-            })
-        } else if (pathname.match(/^\/(s|g)\/[^\/]+/i)) {
-            if (method === 'GET') {
-                return renderError({ status: 400, message: 'Invalid Program ID' }, request)
-            } else {
-                return new Response('Error: Invalid program id', {
-                    status: 400
-                })
-            }
-        } else if (pathname.match(/^\/programs\/[0-9]+(\/|([0-9]+|))(\/|)/i)) {
+                headers: { "content-type": "application/json" }});       
+        }
+        // /g/id endpoint
+        if (pathname.match(/^\/g\/[0-9]+/i)) {
+            // Get the program from the database and send it on it's way
+            const program = await getProgram(pathname.split('/').reverse()[0]);
+            return new Response(JSON.stringify(program), { 
+                headers: { "content-type": "application/json" }});
+        }
+        // /s/id and /g/id invalid ID catching endpoint
+        if (pathname.match(/^\/(s|g)\/[^\/]+/i)) {
+            // GUI error page
+            if (method === 'GET') return renderError({ status: 400, message: 'Invalid Program ID' }, request);
+            // Non-GUI error message
+            return new Response('Error: Invalid program id', {
+                status: 400 });
+        }
+        // /programs/limit/*offset endpoint
+        if (pathname.match(/^\/programs\/[0-9]+(\/|([0-9]+|))(\/|)/i)) {
+            // Split the pathname to get the limit and offset
             let splitPath = pathname.split('/')
-            if (splitPath[splitPath.length-1] === '') {
-                splitPath.splice(-1, 1)
-            }
-            console.log(splitPath)
+            if (splitPath[splitPath.length-1] === '') splitPath.splice(-1, 1);
+            // Get and send the programs
             return new Response(getPrograms(splitPath[2], Number(splitPath[3]) !== NaN ? splitPath[3] : 0), {
-                headers: { "content-type": "application/json" }
-            })
-        } else if (method === "GET") {
-            if (pathname === "/favicon.ico") {
-                return serveImage('assets/favicon.ico')
-            } else if (pathname === "/android-chrome-512x512.png") {
-                return serveImage('assets/android-chrome-512x512.png');
-            } else if (pathname === "/android-chrome-192x192.png") {
-                return serveImage('assets/android-chrome-192x192.png');
-            } else if (pathname === "/apple-touch-icon.png") {
-                return serveImage('assets/apple-touch-icon.png');
-            } else if (pathname === "/favicon-16x16.png") {
-                return serveImage('assets/favicon-16x16.png')
-            } else if (pathname === "/favicon-32x32.png") {
-                return serveImage('assets/favicon-32x32.png')
-            } else if (pathname === "/site.webmanifest") {
-                return new Response(Bun.file('assets/site.webmanifest'))
-            } else if (pathname === "/add") {
+                headers: { "content-type": "application/json" }});
+        }
+        // GET only requests
+        if (method === "GET") {
+            // Home page
+            if (pathname === "/") return renderPage({ title: 'Khan Academy Program Archive', stylesheet: '/css/home.css', body: Home }, request);
+            // Any image in the assets folder
+            if (pathname.match(/assets\/[a-z0-9-_]+\.(svg|png|jpeg|ico)/i) || pathname.match(/css\/[a-z0-9-_]+\.css/i)) {
+                return new Response(Bun.file(pathname.slice(1)));
+            }
+            // Favicons
+            if (pathname === "/favicon.ico")                return serveImage('assets/favicon.ico');
+            if (pathname === "/android-chrome-512x512.png") return serveImage('assets/android-chrome-512x512.png');
+            if (pathname === "/android-chrome-192x192.png") return serveImage('assets/android-chrome-192x192.png');
+            if (pathname === "/apple-touch-icon.png")       return serveImage('assets/apple-touch-icon.png');
+            if (pathname === "/favicon-16x16.png")          return serveImage('assets/favicon-16x16.png');
+            if (pathname === "/favicon-32x32.png")          return serveImage('assets/favicon-32x32.png');
+            if (pathname === "/site.webmanifest")           return new Response(Bun.file('assets/site.webmanifest'));
+            // Add page
+            if (pathname === "/add") {
                 const params = URLSearchParams(url.split('?')[1]) // Get URL parameters
                 return (async (params, request) => {
                     let props = {}; // Will be passed as props to the Add component
@@ -177,13 +178,11 @@ export default {
                     // Render and return the add page
                     return await renderPage({ title: 'Results | KAP Archive', stylesheet: '/css/add.css', body: Add, props }, request);
                 })(params, request)
-            } else if (pathname === "/") {
-                return renderPage({ title: 'Khan Academy Program Archive', stylesheet: '/css/home.css', body: Home }, request)
-            } else if (pathname.match(/assets\/[a-z0-9-_]+\.(svg|png|jpeg|ico)/i) || pathname.match(/css\/[a-z0-9-_]+\.css/i)) {
-                return new Response(Bun.file(pathname.slice(1)))
             }
+            // GUI 404 page
             return renderError({ status: 404, message: '404 Not Found' }, request)
         }
+        // NON-GUI 404 page
         return new Response('Error: 404 not found', { status: 404})
     },
 }
