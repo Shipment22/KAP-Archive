88a9da6dfeaa71fac8c835b1d92067a283fdfe73
Ben <ben@familysafeinternet.com>
2023-03-11 20:24:55 -0500

    Fix program grid for when no programs are given

--- src/components/programsGrid.jsx
+++ src/components/programsGrid.jsx
@@ -1,13 +1,19 @@
 import Program from '../components/program';
 
 function Header(props) {
-	return (
+	return props.programs.length > 0 ? (
 	<div className="programs-grid">
         {props.programs.map(program => (
             <Program key={program.id} {...program}/>
         ))}
 	</div>
-	)
+	) : (
+	<p>
+		<h2 className="red">No programs to display</h2>
+		<code>props.programs.length</code>
+		<br/> is less than 0
+	</p>
+    )
 }
 
 export default Header
