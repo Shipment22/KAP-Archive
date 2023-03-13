2d61778602bfcd677bdccb3a484158e8ad6b57e3
Ben <ben@familysafeinternet.com>
2023-03-13 16:13:34 -0400

    Add comments explaining what everything does

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -10,17 +10,24 @@ function Search(props) {
 	return (
 	<main className="Main">
 		<h1>Search Programs</h1>
+		{/* The search form with a message at the top asking you to search again */}
 		<div className="surface">
 			<h2>Search again?</h2>
 			<SearchForm/>
 		</div>
+		{/* When props.programs.length is less than 0 display a message telling you that */}
 		{props.programs.length > 0 ? (
+			// Display any results found
 			<div>
 				<h2>Results</h2>
 				<ProgramsGrid programs={props.programs}/>
 			</div>
 		) : (
-			<div style={{paddingBlock: '3rem', minHeight: 'calc(100dvh - 72rem)'}}>
+			<div style={{
+					paddingBlock: '3rem', 			  /* Padding for visual appeal */
+					minHeight: 'calc(100dvh - 72rem)' /* Keep the page at the mostly right height */
+				}}>
+				{/* No programs found, try again? message */}
 				<h2 className="red">No Programs Found</h2>
 				<p>
 					Maybe try searching again? Or if you think there's a problem with the KAP Archive you can report an <a href="https://github.com/shipment22/kap-archive/issues">issue on Github</a>.
