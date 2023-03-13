7a8e6c720b50a2b542727c24af6b5803c40397b4
Ben <ben@familysafeinternet.com>
2023-03-13 16:08:50 -0400

    Add a search form at the top of the search page to quickly and easily search again, as well as add a better "No Programs Found" message when props.programs.length is less than 0

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,5 +1,6 @@
 import {getProgramsNoString} from '../libs/retrievePrograms';
 import ProgramsGrid from '../components/programsGrid';
+import SearchForm from '../components/searchForm';
 function Search(props) {
 	const page = Number(props.page);
 	// Make sure page isn't negative or NaN
@@ -9,7 +10,24 @@ function Search(props) {
 	return (
 	<main className="Main">
 		<h1>Search Programs</h1>
-		<ProgramsGrid programs={props.programs}/>
+		<div className="surface">
+			<h2>Search again?</h2>
+			<SearchForm/>
+		</div>
+		{props.programs.length > 0 ? (
+			<div>
+				<h2>Results</h2>
+				<ProgramsGrid programs={props.programs}/>
+			</div>
+		) : (
+			<div style={{paddingBlock: '3rem', minHeight: 'calc(100dvh - 72rem)'}}>
+				<h2 className="red">No Programs Found</h2>
+				<p>
+					Maybe try searching again? Or if you think there's a problem with the KAP Archive you can report an <a href="https://github.com/shipment22/kap-archive/issues">issue on Github</a>.
+				</p>
+			</div>
+		)}
+		
 	</main>
 	);
 }
