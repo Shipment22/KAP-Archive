8fa44f021038d884dc338835a4def8dc2345d775
Ben <ben@familysafeinternet.com>
2024-02-26 11:24:29 -0500

    Footer overhaul

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,27 +1,30 @@
 function Footer() {
 	return (
 		<footer className="Footer">
-			<div className="footer_inner-wrapper">
-				<h2>Pages</h2>
+			<div className="footer_inner-wrapper main-width">
 				<ul>
+				    <h2>Pages</h2>
 					<li><a href="/">Home</a></li>
 					<li><a href="/browse" className="rosewater">Browse</a></li>
 					<li><a href="/search" className="mauve">Search</a></li>
 					<li><a href="/add" className="maroon">Add</a></li>
-					<li><a href="/about" className="yellow">About</a></li>
-					<li><a href="/report" className="red">Report</a></li>
-					<li><a href="/admin" className="blue">Admin</a></li>
 				</ul>
-				<h2>Other similar and cool projects</h2>
-				<ul>
-					<li><a href="https://willard.fun/">Willard.fun</a></li>
-					<li><a href="https://vxsacademy.org/computer-programming/browse">Vexcess Academy</a></li>
-					<li><a href="https://khanalytics.bhavjit.com/">Khanalytics</a></li>
-					<li><a href="https://search.kestron.software/">Kestrogle</a></li>
+                <ul>
+                    <h2>Resources</h2>
+					<li><a href="/request_by_code" className="mauve">Request Hidden/Deleted Program</a></li>
+					<li><a href="/report" className="red">Report a Program</a></li>
+					<li><a href="https://github.com/Shipment22/KAP-Archive" className="sapphire">Github Repo</a></li>
+                </ul>
+                <ul>
+                    <h2>Cool Projects</h2>
+					<li><a href="https://vxsacademy.org/computer-programming/browse" style={{ color: '#45ed84' }}>Vexcess Academy</a></li>
+					<li><a href="https://khanalytics.bhavjit.com/" style={{ color: '#41f0c0' }}>Khanalytics</a></li>
+					<li><a href="https://willard.fun/" className="sapphire">Willard.fun</a></li>
+					<li><a href="https://search.kestron.software/" className="sapphire">Kestrogle</a></li>
 				</ul>
-				<h2>Credits</h2>
 				<ul>
-					<li><a href="https://iconarchive.com/show/flatastic-1-icons-by-custom-icon-design/information-icon.html">Info Icon</a> (<a href="http://www.customicondesign.com/">Custom Icon Design</a>)</li>
+				    <h2>Credits</h2>
+					<li><a href="https://iconarchive.com/show/flatastic-1-icons-by-custom-icon-design/information-icon.html">Info Icon</a> (<a href="http://www.customicondesign.com/" className="sapphire">Custom Icon Design</a>)</li>
 					<li>
 						<a href="https://iconarchive.com/show/small-n-flat-icons-by-paomedia/sign-error-icon.html">Error Icon</a>
 					</li>
@@ -32,10 +35,10 @@ function Footer() {
 						<a href="https://iconarchive.com/show/small-n-flat-icons-by-paomedia/key-icon.html">Key Icon</a>
 				  </li>
 					<li>
-						(<a href="http://www.paomedia.com/">paomedia</a>, <a href="https://github.com/paomedia">Github</a>)
+						(<a href="http://www.paomedia.com/" className="sapphire">paomedia</a>, <a href="https://github.com/paomedia" className="sapphire">Github</a>)
 					</li>
 					<li>
-						<a href="https://iconarchive.com/show/papirus-status-icons-by-papirus-team/view-private-icon.html">View private Icon</a> (<a href="https://github.com/PapirusDevelopmentTeam">Papirus Development Team</a>)</li>
+						<a href="https://iconarchive.com/show/papirus-status-icons-by-papirus-team/view-private-icon.html">View private Icon</a> (<a href="https://github.com/PapirusDevelopmentTeam" className="sapphire">Papirus Development Team</a>)</li>
 				</ul>
 			</div>
 		</footer>
