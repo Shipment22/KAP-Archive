8a9c277b945d6d30667713c97e7178bbdabc87f5
Ben <ben@familysafeinternet.com>
2024-03-04 23:46:24 -0500

    Update ids placeholder text

--- src/components/addForm.jsx
+++ src/components/addForm.jsx
@@ -5,7 +5,7 @@ function AddForm() {
 	return (
 		<form action="/add" className="add-form">
 			<label htmlFor="ids">Program IDs</label>
-			<textarea name="ids" className="input" rows="6" placeholder="IDs and/or URL separeted by new lines" title="IDs and/or URL separeted by new lines"></textarea>
+			<textarea name="ids" className="input" rows="6" placeholder="IDs and/or URL separeted by commas or new lines" title="IDs and/or URL separeted by commas or new lines"></textarea>
 	        <div className="info">
 	            <img src={infoIcon} alt="Information Icon" width="22px" height="22px"/>
 	            <p>
