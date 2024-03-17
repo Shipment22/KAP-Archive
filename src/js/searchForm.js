0b93131b4f00296032421d7efedc36128bdd28fa
Ben <ben@familysafeinternet.com>
2024-03-17 17:58:40 -0400

    Fix title attribute for the search form expander button and add animation on expansion and unexpansion

--- src/js/searchForm.js
+++ src/js/searchForm.js
@@ -1,20 +1,21 @@
 window.addEventListener("load", e => {
     console.log("Hello from searchForm.js!");
+    let searchForm = document.getElementsByClassName("search-form")[0];
     
     [...document.getElementsByClassName("search-form-expander-button")].forEach(expander => {
         expander.onclick = function(e) {
             e.preventDefault();
-            [...this.parentElement.getElementsByClassName("search-form-expanded")].forEach(expandable => {
-                if (expandable.style.display !== "") {
-                    expandable.style.display = "";
-                    this.textContent = "Show Less";
-                    window.localStorage.setItem("searchForm-expanded", 1);
-                    return;
-                }
-                expandable.style.display = "none";
-                this.textContent = "Show More";
-                window.localStorage.setItem("searchForm-expanded", 0);
-            });
+            if (searchForm.classList.contains("unexpanded")) {
+                searchForm.classList.remove("unexpanded");
+                this.textContent = "Show Less";
+                this.title = "Show less fields";
+                window.localStorage.setItem("searchForm-expanded", 1);
+                return;
+            }
+            searchForm.classList.add("unexpanded");
+            this.textContent = "Show More";
+            this.title = "Show more fields";
+            window.localStorage.setItem("searchForm-expanded", 0);
         };
         if (window.localStorage.getItem("searchForm-expanded") == 0) {
             expander.click();
