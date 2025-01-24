8ddbeabe70ddf8d9337e173919da89bda515757d
Shipment22 <89107348+Shipment22@users.noreply.github.com>
2025-01-24 11:20:09 -0500

    feat(program cards):
    Better tooltips and lazy loading

--- src/components/programsGrid.jsx
+++ src/components/programsGrid.jsx
@@ -3,9 +3,10 @@ import Program from '../components/program';
 function Header(props) {
 	return props.programs.length > 0 ? (
 	<div className="programs-grid">
-        {props.programs.map(program => (
-            <Program key={program.id} {...program}/>
-        ))}
+        {props.programs.map(p => {
+            if (props.programs.indexOf(p) >= 12) p.lazy = "true";
+            return (<Program key={p.id} {...p}/>);
+        })}
 	</div>
 	) : (
 	<p>
